96b0edcfac85b5e66b7b92920cac4ebc659d1e40
simenbs <141590049+simenbs@users.noreply.github.com>
2023-08-10 10:35:38 +0200

    UnfinishedJavaproject

---
+++ IntroJS/Script.js
@@ -0,0 +1,28 @@
+function show1() {
+    blankAll()
+
+
+    document.getElementById('simensTag1').innerHTML = /*HTML*/`
+
+<h3>Aaa</h3>
+Hei, Hei, Hei.
+
+`  ;
+
+}
+function show2() {
+
+    blankAll()
+
+    document.getElementById('simensTag2').innerHTML = /*HTML*/`
+
+<h3>Aaa</h3>
+Hei, Hei, Hei.
+
+`  ;
+}
+
+function blankAll() {
+    document.getElementById('simensTag1').innerHTML = '';
+    document.getElementById('simensTag2').innerHTML = '';
+}
